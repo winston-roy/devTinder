1e580e032998ed8dd23a228f34976d7f01f28a8e
Winston <pashanwinsty1998@gmail.com>
2025-05-29 08:59:37 +0530

    changes

---
+++ src/config/database.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    await mongoose.connect('mongodb+srv://winstonroy:winstonroy@cluster0.7ies1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+}
+
+module.exports = connectDB;
