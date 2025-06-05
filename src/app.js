0d5c2155c585f2a6aa7f88387b40944b91112255
winston.pashan <winston.pashan@99games.in>
2025-06-05 14:42:14 +0530

    data sanitization and schema validation

--- src/app.js
+++ src/app.js
@@ -5,34 +5,77 @@ const { connectDB } = require('./config/database');
 const { UserModel } = require("./models/user")
 
 
+app.use(express.json())
+
+
 app.post("/user/signup", async (req, res) => {
     try {
-        let userData = {
-            firstName: "Winston",
-            lastName: "Pashan",
-            email: "winston.pashan@gmail.com",
-            age: 26,
-            gender: "male"
-        }
+        let userData = req.body;
+
         const user = new UserModel(userData);
         await user.save();
-        res.send("User created successfully!!")
-    } catch (err) {
-        res.status(500).send("Error while signup!!!")
+        res.status(200).send({
+            "message": 'User created successfully!!',
+            "data": user
+        })
+    } catch (error) {
+        res.status(400).json({
+            message: error.message,
+        });
     }
+})
 
+app.patch("/user/edit/:userId", async (req, res) => {
+    try {
+        const _id = req.params?.userId;
+        const data = req.body;
 
+        const ALLOWED_UPDATES = ["age", "firstName", "lastName", "skills"];
+
+        const isUpdateAllowed = Object.keys(data).every((k) => {
+            return ALLOWED_UPDATES.includes(k)
+        })
+
+        if (!isUpdateAllowed) {
+            throw new Error("Update not allowed!!!", isUpdateAllowed)
+        }
+
+        if (data?.skills.length > 10) {
+            throw new Error("Skills should not be more than 10")
+        }
+
+        const isUserExists = await UserModel.findById({ _id });
+
+        if (!isUserExists) {
+            throw new Error("User not found")
+        }
+
+        const user = await UserModel.findByIdAndUpdate(_id, data, {
+            returnDocument: "after",
+            runValidators: true
+        })
+
+        res.status(200).send({
+            message: "User Updated successfully",
+            data: user
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            message: error.message,
+        });
+    }
 })
 
 
 connectDB()
     .then(() => {
         console.log('database connection successfull')
-        app.listen(4200, (err) => {
+        app.listen(7777, (err) => {
             if (err) {
                 console.error('Failed to start server:', err);
             } else {
-                console.log('Server is listening on port 4200');
+                console.log('Server is listening on port 7777');
             }
         });
     })
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,71 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        require: true
     },
     lastName: {
-        type: String
+        type: String,
+        require: true
     },
     email: {
-        type: String
+        type: String,
+        require: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Enter Correct Email ID " + value)
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        require: true,
+        min: [18, 'Must be at least 18, got {VALUE}'],
+        maxlength: 2
     },
     gender: {
-        type: String
+        type: String,
+        require: true,
+        enum: {
+            values: ['male', 'female', 'other'],
+            message: `enum validator failed for the path {PATH} with value {VALUE}`
+        }
+    },
+    phoneNumber: {
+        type: String,
+        validate(value) {
+            if (!validator.isMobilePhone(value)) {
+                throw new Error("Enter Valid Phone Number " + value)
+            }
+        }
     },
     password: {
-        type: String
+        type: String,
+        require: true,
+        validate(value) {
+            if (!validator.isStrongPassword(value, { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1, returnScore: false, pointsPerUnique: 1, pointsPerRepeat: 0.5, pointsForContainingLower: 10, pointsForContainingUpper: 10, pointsForContainingNumber: 10, pointsForContainingSymbol: 10 })) {
+                throw new Error("Password need to have { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1}")
+            }
+        }
     },
-})
+    profilePic: {
+        type: String,
+        default: 'https://ca.slack-edge.com/T01H2UQCP3J-U01GF8VNEAH-b460649f4b2d-48',
+        validate(value) {
+            if (!validator.isURL(value)) {
+                throw new Error("Enter Valid Profile URL " + value)
+            }
+        }
+    },
+    skills: {
+        type: [String]
+    }
+}, { timestamps: true })
 
 const UserModel = mongoose.model("User", userSchema);
 
