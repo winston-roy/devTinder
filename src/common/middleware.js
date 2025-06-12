23737a155b8702e33377b6ce35fb15b903059f21
winston.pashan <winston.pashan@99games.in>
2025-06-12 12:21:00 +0530

    cors options

--- src/common/middleware.js
+++ src/common/middleware.js
@@ -3,7 +3,10 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 function loadMiddlerware(app) {
-    app.use(cors);
+    app.use(cors({
+        origin: 'http://localhost:5174',
+        credentials: true
+    }));
     app.use(express.json());
     app.use(cookieParser());
 }
