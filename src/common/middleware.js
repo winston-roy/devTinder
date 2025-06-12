cfc47f45097e75816e671cc49caa198d882edb9b
winston.pashan <winston.pashan@99games.in>
2025-06-12 08:15:12 +0530

    axios

--- src/common/middleware.js
+++ src/common/middleware.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 function loadMiddlerware(app) {
+    app.use(cors);
     app.use(express.json());
     app.use(cookieParser());
 }
