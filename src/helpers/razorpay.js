88b1d23f4cfa0cf8c4065cec37bcdc06a0a3f309
winston.pashan <winston.pashan@99games.in>
2025-07-04 22:42:40 +0530

    added log

--- src/helpers/razorpay.js
+++ src/helpers/razorpay.js
@@ -1,5 +1,6 @@
 const Razorpay = require('razorpay');
-
+console.log('key id', process.env.RAZOR_PAY_KEY_ID)
+console.log('key secret', process.env.RAZOR_PAY_KEY_SECRET)
 var instance = new Razorpay({
     key_id: process.env.RAZOR_PAY_KEY_ID,
     key_secret: process.env.RAZOR_PAY_KEY_SECRET
